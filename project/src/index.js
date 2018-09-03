28128e230ba448f7d3069a6c8483d0a23b4b6f4d
Ruslan Bagautdinov <molodoybaja@gmail.com>
2018-09-03 23:05:20 +0500

    commit

--- project/src/index.js
+++ project/src/index.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Cookies from 'js-cookie';
 import { AppContainer } from 'react-hot-loader';
+import { routerMiddleware } from 'react-router-redux';
 
+import DevTools from './components/DevTools';
 import Client from './client';
 
-// class Client extends Component {
-//   render() {
-//     return <h1>Hello world!!!!</h1>;
-//   }
-// }
+import history from './history';
+import configureStore from './store';
+import { testToken } from './actions';
+
+const store = configureStore(window.REDUX_INITIAL_STATE, 'browser', routerMiddleware(history));
 
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
-      <Component />
+      {Component}
     </AppContainer>,
     document.getElementById('root'),
   );
 };
 
-render(Client);
+const token = Cookies.get('token');
+store.dispatch(testToken(token)).then(() => {
+  render(<Client store={store} history={history}/>);
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  ReactDOM.render(<DevTools store={store} />, document.getElementById('dev-tools'));
+}
 
 if (module.hot) {
   module.hot.accept();
